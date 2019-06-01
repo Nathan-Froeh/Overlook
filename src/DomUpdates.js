ebcecbb65f3b7a2e80873881220ae8ce5422d95a
Nathan <nathan.froeh@gmail.com>
2019-06-01 09:27:33 -0600

    Add customer no order history message

--- src/DomUpdates.js
+++ src/DomUpdates.js
@@ -32,7 +32,11 @@ let DomUpdates = {
   loadUserInfo(roomServiceRepo, userRepo, bookingsRepo, roomsRepo, roomService) {
     $('.guest__name').text(userRepo.currentUser.name)
     $('.guest__id').text(userRepo.currentUser.id)
-    $('.all__personal__orders').text(roomService.allOrders)
+    if (roomService.allOrders.length === 0) {
+      $('.all__personal__orders').text('Customer has no order history')
+    } else {
+      $('.all__personal__orders').text(roomService.allOrders)
+    }
     $('.personal__spent__by__day').text(roomService.todaysTotal(today))
     $('.personal__grand__total_spent').text(roomService.totalOrders())
     console.log(roomService.allOrders)
